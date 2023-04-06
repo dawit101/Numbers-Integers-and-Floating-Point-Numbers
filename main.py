90ee0ec48d1ff28c65f9bf7fe613c8c205de11c8
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 05:36:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Fundamental Data Type
+#int = integer, which is just a whole number
+#float = floating point number, which means number with decimal point
+#type(...) means to see what kind of datatype it is
+
+print(type(2 + 4)) #6, so its int
+print(type(2 - 4)) #-2, so its int
+print(type(2 / 4)) #0.5, so its float
+
+print(type(0.00001)) #0.00001 is float data type
+
+print(type(20 + 1.1)) #21.1, so its float
+
+print(type(9.9 +1.1)) #11.0, so its a float
+
+print(2 ** 3) #2 to the 3rd power, 2^3 = 8
+
+print(2 // 4) #divides and rounds down to next integer
+print(6 // 4) #divides and rounds down to next integer
+
+print(6 % 4) # % = modulo, is used to find remainder of division, so 6/4 has a remainder of 2
+print(8 % 3) #8/3 is 2 with the remainder of 2
